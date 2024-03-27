4dbdf4984485949ea9ae35dddd2c60f73a7159b8
Zachary Lentz <zlentz@slac.stanford.edu>
2024-03-27 10:08:30 -0700

    ENH: add failure state and override for python env

--- scripts/activate_python.sh
+++ scripts/activate_python.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 # This is meant to be sourced.
 # Helper script for activating the correct python environment.
+# Sets a default env or you can provide your own activate path for testing
 
-source /cds/group/pcds/pyps/conda/venvs/ansible/bin/activate
+DEFAULT_ENV=/cds/group/pcds/pyps/conda/venvs/ansible/bin/activate
+
+if [ -f "${ANSIBLE_PYTHON_ACTIVATE:=${DEFAULT_ENV}}" ]; then
+  source "${ANSIBLE_PYTHON_ACTIVATE}"
+else
+  echo "No Python activation script found at ${ANSIBLE_PYTHON_ACTIVATE}"
+  return 1
+fi
