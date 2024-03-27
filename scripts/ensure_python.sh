4d45111f31bb3c5cb3afdf1abab4b55040ffc52e
Zachary Lentz <zlentz@slac.stanford.edu>
2024-03-27 09:55:27 -0700

    MAINT: simplify the python env script, checking is too slow

--- scripts/ensure_python.sh
+++ scripts/ensure_python.sh
@@ -1,38 +1,5 @@
 #!/bin/bash
 # This is meant to be sourced.
-# Helper script for ensuring we have a suitable python environment loaded.
-# Needs to make sure the following binaries are available:
-#   - ansible-playbook
-# Needs to make sure the following ansible modules are available:
-#   - community.general.xml
-#   - community.general.sysrc
-# Needs to make sure the following libraries are importable:
-#   - lxml
-#   - ruamel.yaml
-# If any of these are missing, try to load the prepared python env.
+# Helper script for activating the correct python environment.
 
-ISSUES=()
-
-if [ ! -x "$(command -v ansible-playbook)" ]; then
-  ISSUES+=( "Missing ansible-playbook" )
-elif [ ! -x "$(command -v ansible-doc)" ]; then
-  ISSUES+=( "Missing ansible-doc" )
-else
-  for module in community.general.xml community.general.sysrc; do
-    if [ "$(ansible-doc -j "${module}")" = "{}" ]; then
-      ISSUES+=( "Missing ${module} ansible module" )
-    fi
-  done
-fi
-if [ ! -x "$(command -v python)" ]; then
-  ISSUES+=( "Missing Python!" )
-else
-  for module in lxml ruamel.yaml; do
-    if ! python -c "import ${module}"; then
-      ISSUES+=( "Missing ${module} python module" )
-    fi
-  done
-fi
-for iss in "${ISSUES[@]}"; do
-    echo "${iss}"
-done
+source /cds/group/pcds/pyps/conda/venvs/ansible/bin/activate
